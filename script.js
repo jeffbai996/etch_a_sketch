71068c01df2fcbe2ef645130e506935ab67b204d
jeffbai996 <j.bai@wustl.edu>
2022-07-21 13:55:10 -0700

    Reworked createGrid() to reset the grid each time a new one is created

--- script.js
+++ script.js
@@ -2,13 +2,20 @@
 const container = document.getElementById("grid-container");
 
 function createGrid(rows) {
+    const existingGrid = document.querySelectorAll('.grid-item');
+    existingGrid.forEach(grid => {
+        grid.remove();
+    })
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', rows);
     for (i = 0; i < (rows * rows); i++) {
         let gridItem = document.createElement("div");
-        // gridItem.innerText = (i + 1);
+        gridItem.addEventListener("mouseover", function(){
+            gridItem.style.backgroundColor = 'black';
+        })
         container.appendChild(gridItem).className = "grid-item";
     }
 }
 
-createGrid(64);
+// create starting 16x16 grid
+createGrid(32);
